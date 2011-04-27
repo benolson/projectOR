219372c82b1b0fb0531428f4e010cdaa6e4cc8c0
Ben Olson <benolson@stanford.edu>
2011-04-27 01:07:15 -0700

    added basic calibration image generator

---
+++ Code/matlab/gen_calibrate_image.m
@@ -0,0 +1,27 @@
+function [img] = gen_calibrate_image(locations,colors)
+
+img = zeros(800,1280,3);
+
+
+function [img] = draw_circle(img_in,x,y,radius,color)
+    img = img_in;
+    for i=x-radius:x+radius
+        for j=y-radius:y+radius
+
+            if (i>0 && i<=1280 && ...
+                j>0 && j<=1280 && ...
+                ( (i-x)^2 + (j-y)^2 )^.5 < radius)
+                img(j,i,:) = color;
+            end
+        end
+    end
+end
+
+
+for i=1:size(locations,1)
+    img = draw_circle(img,locations(i,1),locations(i,2),12,colors(i,:));
+end
+
+
+
+end
